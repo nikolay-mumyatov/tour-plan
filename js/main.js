f2363d256004de460b339f867b5dcc12e6a29627
nikolay-mumyatov <nikolay.mumyatov@yandex.ru>
2021-03-08 15:22:51 +0300

    Верстка слайдера в блоке hotel-info.

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper(".swiper-container", {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".slider-button--next",
+    prevEl: ".slider-button--prev",
+  },
+
+  autoplay: {
+    // delay: 5000,
+  },
+});
