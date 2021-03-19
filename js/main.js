ddfb8aa6d1302d625f9e3d3353028ad130d102e7
nikolay-mumyatov <nikolay.mumyatov@yandex.ru>
2021-03-19 19:49:49 +0300

    Закрытие по кнопке ESC.

--- js/main.js
+++ js/main.js
@@ -36,27 +36,33 @@ const reviews = new Swiper(".reviews-slider-container", {
 // Burger menu
 
 var menuBtn = document.querySelector(".burger"),
-    burgerLine = document.querySelectorAll(".burger__line");
-    
+  burgerLine = document.querySelectorAll(".burger__line");
+
 burgerLine.forEach(function (line) {
   menuBtn.addEventListener("click", function () {
     line.classList.toggle("burger__line-active");
-    document.querySelector(".header-nav").classList.toggle("header-nav__visible");
+    document
+      .querySelector(".header-nav")
+      .classList.toggle("header-nav__visible");
   });
 });
-
-
 
 // Modal window
 
 let modalWindow = document.querySelector(".modal"),
-    closeBtn = document.querySelector(".modal-close");
-    
+  closeBtn = document.querySelector(".modal-close");
 
 // Прослушивание документа на нажатие. Если нажали на элемент с классом .modal-btn то произойдет событие.
 document.addEventListener("click", function (e) {
-  const target = e.target
+  const target = e.target;
   if (target.matches(".modal-btn")) {
+    modalWindow.classList.toggle("modal-active");
+  }
+});
+
+// Закрытие по кнопе ESC
+$(document).keydown(function (eventObject) {
+  if (eventObject.which == 27) {
     modalWindow.classList.toggle("modal-active");
   }
 });
